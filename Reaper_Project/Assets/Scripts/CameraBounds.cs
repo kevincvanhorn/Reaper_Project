4511df2aa8f398d80a34ecccb3f1d89dab6e6175
Kevin VanHorn <kevin.c.vanhorn@gmail.com>
2017-07-06 01:31:31 -0500

    Camera Follow: Initial Class and Bounding Collider Base-Code

---
+++ Reaper_Project/Assets/Scripts/CameraBounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter2D(Collider2D coll) {  
+    }
+
+    void OnTriggerStay2D(Collider2D coll) {
+    }
+
+    void OnTriggerExit2D() {
+    }
+}
---
+++ Reaper_Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform player;
+    public Vector3 offset;
+    float width, height, playerHeight;
+    Vector3 worldDimensions, center, focalpoint;
+
+	// Use this for initialization
+	void Start () {
+        GameObject myPlayer = GameObject.Find("Player");
+        player = myPlayer.transform;
+        playerHeight = 2;
+
+        /* Camera Properties*/
+        //Vector2 topRightCorner = new Vector2(1, 1);
+        //Vector2 edgeVector = Camera.main.ViewportToWorldPoint(topRightCorner); // Make 2D -> 3D vector.
+        worldDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, -10));
+        width = worldDimensions.x;
+        height = worldDimensions.y;
+
+        offset.x = 0;// width;
+        offset.y = 0;// height / 2;//height;
+        offset.z = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //print(player.transform.position + " " + transform.position);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y+offset.y, -10);
+        //transform.position = new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, transform.position.z + offset.z);
+
+	}
+}
