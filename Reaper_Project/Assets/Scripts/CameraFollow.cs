de2676cd46bada4590819298b3975142396bc6e3
Kevin VanHorn <kevin.c.vanhorn@gmail.com>
2017-07-08 14:57:49 -0500

    Camera Base Follow.

--- Reaper_Project/Assets/Scripts/CameraFollow.cs
+++ Reaper_Project/Assets/Scripts/CameraFollow.cs
@@ -7,130 +7,45 @@ public class CameraFollow : MonoBehaviour {
     public Vector3 offset;
     float width, height, playerHeight;
     Vector3 worldDimensions, center, focalpoint;
-    float camX, camY, camZ;
     Character myPlayer;
 
-    /* Colliders */
-    public CameraCollisionsInfo collisions;
-    public List<GameObject> ChildrenColliders;
-    public CameraBounds topCollider;
-    public CameraBounds botCollider;
-    public CameraBounds leftCollider;
-    public CameraBounds rightCollider;
+    Transform cam;
 
 	// Use this for initialization
 	void Start () {
-
-        /* CollisionInfo Deafults */
-       collisions.isTouchingTop = false;
-       collisions.isTouchingBot = false;
-       collisions.isTouchingRight = false;
-       collisions.isTouchingLeft = false;
-       collisions.isMovingRight = false;
-       collisions.isMovingUp = false;
-
-
-        /* Set child colliders. */
-        foreach (Transform child in transform) {
-            if (child.tag == "CameraBoundingChild") {
-                ChildrenColliders.Add(child.gameObject);
-            }
-        }
-
-        topCollider = ChildrenColliders[0].GetComponent<CameraBounds>();
-        botCollider = ChildrenColliders[1].GetComponent<CameraBounds>();
-        leftCollider = ChildrenColliders[2].GetComponent<CameraBounds>();
-        rightCollider = ChildrenColliders[3].GetComponent<CameraBounds>();
-
-        topCollider.OnEdgeEnter += onTopCollisionEnter;
-        botCollider.OnEdgeEnter += onBotCollisionEnter;
-        leftCollider.OnEdgeEnter += onLeftCollisionEnter;
-        rightCollider.OnEdgeEnter += onRightCollisionEnter;
-
-        topCollider.OnEdgeExit += onTopCollisionExit;
-        botCollider.OnEdgeExit += onBotCollisionExit;
-        leftCollider.OnEdgeExit += onLeftCollisionExit;
-        rightCollider.OnEdgeExit += onRightCollisionExit;
-
-
         GameObject myPlayerObj = GameObject.Find("Player");
         if(myPlayerObj != null){
             myPlayer = myPlayerObj.GetComponent<Character>();
         }
-
-        player = myPlayer.transform;
-        playerHeight = 2;
 
         /* Camera Properties*/
         worldDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, -10));
         width = worldDimensions.x;
         height = worldDimensions.y;
 
-        offset.x = 0;// width;
-        offset.y = 0;// height / 2;//height;
-        offset.z = 0;
+        cam = this.transform;
+        player = myPlayer.transform;
 
-        camX = 0.1f;
-        camY = 11.1f;
-        camZ = -10;
+        print(cam.position + " " + player.position);
+
+        // TODO: Current Offset adds additional values somehow to the offset ontop of the below calculations.
+        offset.x = 0;//offset.x = cam.position.x - player.position.x; // Allow for player starting at any point to maintain displacement.
+        offset.y = 0;//offset.y = cam.position.y - player.position.y;
+        offset.z = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        print(collisions.isTouchingLeft);
+        Vector3 camPos = cam.position; // Instantiating can be costly, copy the struct and reapply to the object.
 
-        if (!collisions.isTouchingLeft) {
-            camX = player.transform.position.x;
-        }
-        //if(!collisions.isTouchingBot){
-            camY = player.transform.position.y;
-        //}
+        camPos.x = player.position.x + offset.y;
+        camPos.y = player.position.y + offset.y;
 
-        transform.position = new Vector3(camX, camY, -10);
+        cam.position = camPos;
 	}
 
 
-    /** Called on Camera collision with boundingBox. **/
-    void onTopCollisionEnter() {
-        collisions.isTouchingTop = true;
-    }
-    void onBotCollisionEnter() {
-        collisions.isTouchingBot = true;
-        print("BOT!");
-    }
-    void onLeftCollisionEnter() {
-        collisions.isTouchingLeft = true;
-        print("LEFT!");
-    }
-    void onRightCollisionEnter() {
-        collisions.isTouchingRight = true;
-        print("RIGHT!");
-    }
-
-    /** Called on Player leaving collision with an object. **/
-    void onTopCollisionExit() {
-        collisions.isTouchingTop = false;
-    }
-    void onBotCollisionExit() {
-        collisions.isTouchingBot = false;
-    }
-    void onLeftCollisionExit() {
-        collisions.isTouchingLeft = false;
-    }
-    void onRightCollisionExit() {
-        collisions.isTouchingRight = false;
-    }
-
 
 
 }
 
-public struct CameraCollisionsInfo {
-    public bool isTouchingTop;
-    public bool isTouchingBot;
-    public bool isTouchingRight;
-    public bool isTouchingLeft;
-
-    public bool isMovingRight;
-    public bool isMovingUp;
-}
